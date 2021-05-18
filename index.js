73f043eee4dcaf1780c9b25db3bf2a5df94d5aa2
Parv <62258708+parv00cypher@users.noreply.github.com>
2021-05-18 23:50:32 +0530

    links updated
    added new links of fb insta

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function copyToClipboard(element) {
     var $temp = $("<input>");
     $("body").append($temp);
@@ -6,12 +6,3 @@ function copyToClipboard(element) {
     document.execCommand("copy");
     $temp.remove();
 }
-=======
-function copyToClipboard(element) {
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val($(element).text()).select();
-    document.execCommand("copy");
-    $temp.remove();
-}
->>>>>>> b8a926abf0dd389816e32fffb102df82a27c54e7
